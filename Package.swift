3c064fab04e94024303e934dce8e149e87a4b338
Imre Mihaly <imihaly@users.noreply.github.com>
2022-10-14 08:51:43 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/imihaly/WebRTC/releases/download/0.0.5/WebRTC-2022-10-13T21-30-46.xcframework.zip",
-            checksum: "9fc36017b827f570fb51895b6e5310b2dfb20aa3f2f373f9a47d202bca040e30"
+            url: "https://github.com/imihaly/WebRTC/releases/download/0.0.6/WebRTC-2022-10-14T06-50-28.xcframework.zip",
+            checksum: "f51e93ba774a340b11352c8dbae43293a924328019d8b4984e3ccea23f83ac88"
         ),
     ]
 )
